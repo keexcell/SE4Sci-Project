0bab8de63bf1fff7a81f4ed4a98a3f14e0063052
Justin Marquez <justin.marquez@wisc.edu>
2025-07-01 10:38:54 -0500

    restore code that was mistakenly removed

--- src/diffeqnsolver/solver.py
+++ src/diffeqnsolver/solver.py
@@ -2,7 +2,11 @@ import abc
 
 class Solver(abc.ABC):
     def __init__(self, func):
+        # Eqn to solve
+        self.f = func
+
         self.coefficients = []
+        self.solution = lambda: None
 
     @abc.abstractmethod
     def solve(self):
