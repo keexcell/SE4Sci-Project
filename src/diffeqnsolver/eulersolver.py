221d1ec9ee3645151f50e65cf410901117d7e7df
Justin Marquez <justin.marquez@wisc.edu>
2025-07-01 13:37:48 -0500

    add docs

--- src/diffeqnsolver/eulersolver.py
+++ src/diffeqnsolver/eulersolver.py
@@ -5,14 +5,32 @@ class EulerSolver(Solver):
     def __init__(self, func):
         super().__init__(func)
 
-    def solve(self, x_0: float, y_0: float, x_1: float, num_steps: int = 100):
+    def solve(self, x_0: float, y_0: float, x_n: float, num_steps: int = 100):
+        """
+        Implements the Euler method to solve functions of the form y'(x) = f(x,y)
+        for y(x). Numerical approximations are made for y(x) for x in the range
+        [x_0, x_n] and are stored in the class's `iterations` variable as a list
+        of tuples. The tuples are in the form of (x, y(x), y'(x)).
+
+        Args:
+            x_0: float
+                Initial x value.
+            y_0: float
+                Initial y value, equivalent to y(x_0).
+            x_n: float
+                Final x value. Numerical approximations of y(x) are made in the
+                range [x_0, x_n].
+            num_steps: int
+                Number of iterations to perform within the given range.
+                (Default: 100)
+        """
         self.iterations = [
             (x_0, y_0, self.f(x_0, y_0))
         ]
 
         x_i = x_0
         y_i = y_0
-        step = (x_1-x_0)/num_steps
+        step = (x_n-x_0)/num_steps
         for _ in range(num_steps):
             x_i += step
             y_i += step * self.iterations[-1][2]
