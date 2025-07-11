0bca014b19ac691df431b549cac36319cfebeeaa
kylam0705 <specialk0705@yahoo.com>
2025-07-11 15:14:38 -0500

    first attempt at plotting

--- src/diffeqnsolver/solver.py
+++ src/diffeqnsolver/solver.py
@@ -1,4 +1,5 @@
 import abc
+import numpy as np
 
 
 class Solver(abc.ABC):
@@ -18,5 +19,29 @@ class Solver(abc.ABC):
         pass
 
     def visualize(self, title: str):
-        pass
+        """
+        Visualizes the results of the solved differential equation
+
+        Args: 
+            title: string 
+                Name of the method used
+            x : array
+            y : array
+        """
+
+        # Function Values: 
+        x = self.iterations[:,0]
+        y = self.iterations[:,1]
+        z = self.iterations[:, -1]
+
+        plt.figure(figsize=(8, 6))
+        ax = fig.add_subplot(projection = '3d') # For graphing f(x,y) = z
+        plt.plot(x, y, z)
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title("Solution to differential equation using " + title + " Method")
+        plt.grid(True)
+        plt.show()
+
+        print("Plots produced")
 
