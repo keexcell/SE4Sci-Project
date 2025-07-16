40502d3251fc6f9127f503d908b2eafdad2f0ad4
Justin Marquez <justin.marquez@wisc.edu>
2025-07-16 14:07:51 -0500

    fix derivative and initial conditions

--- tests/test_infinities.py
+++ tests/test_infinities.py
@@ -18,28 +18,32 @@ def test_divergence(solver_to_test):
     '''
     num_steps = 5000
 
+    x0 = 0
+    y0 = 1
+    xn = math.pi/3
+
     if solver_to_test == 'Euler':
         y_prime = EulerSolver(lambda x, y : y*math.tan(x))
         rel_tol = 0.005
         with pytest.raises(ValueError):
-            y_prime.solve(0.0, 1.0, 2*math.pi, num_steps)
-        y_prime.solve(0.0, 1.0, math.pi/3, num_steps)
+            y_prime.solve(x0, y0, 2*math.pi, num_steps)
+        y_prime.solve(x0, y0, xn, num_steps)
     elif solver_to_test == 'Taylor':
         y_prime = TaylorSolver(lambda x, y : y*math.tan(x))
         rel_tol = 0.005 #tol can be diff btwn Taylor and Euler
         with pytest.raises(ValueError):
-            y_prime.solve(0.1, 1.0, 2*math.pi, num_steps, [lambda x, y : -y*1/(math.sin(x)*math.cos(x))])
-        y_prime.solve(0.1, 1.0, math.pi/3, num_steps,[lambda x, y : -y*1/(math.sin(x)*math.cos(x))])
+            y_prime.solve(x0, y0, 2*math.pi, num_steps, [lambda x, y : y/(math.cos(x)*math.cos(x)) + y*math.tan(x)*math.tan(x)])
+        y_prime.solve(x0, y0, xn, num_steps,[lambda x, y : y/(math.cos(x)*math.cos(x)) + y*math.tan(x)*math.tan(x)])
 
     #there's cases where cos = 0 so y = 1/0 and there will be an error
     #test if it overwrites first .solve with another. this time closed interval
     y_prime_solutionlist = y_prime.iterations
-    assert math.isclose(y_prime_solutionlist[-1][0], math.pi/3)
+    assert math.isclose(y_prime_solutionlist[-1][0], xn)
 
     y_prime.visualize("Eueler", "ytan(x)")
 
     for step in range(num_steps):
-        angle = angle_maker(step, angle_length = math.pi/3, num_steps = num_steps)
+        angle = angle_maker(step, angle_length = xn, num_steps = num_steps)
         assert check_tuple_close(y_prime_solutionlist[step], 
                                 (angle, 1/math.cos(angle), math.tan(angle)/math.cos(angle)), rel_tol)
     
