29fecda9cab3bc45a7702bffebedf1e487192341
Kayleigh Excell <kexcell@wisc.edu>
2025-07-16 17:40:03 -0500

    Passed the non-linear test

--- tests/test_multiterm.py
+++ tests/test_multiterm.py
@@ -28,7 +28,7 @@ def test_linear(solver_to_test):
         y_prime = EulerSolver(f)
         rel_tol = 0.005
         y_prime.solve(x0, y0, xn, num_steps)
-        y_prime.visualize("Euler")
+        y_prime.visualize("Euler", r"$6x^2 - 3x^2y$")
     elif solver_to_test == "Taylor":
         y_prime = TaylorSolver(f)
         rel_tol = 0.005 #tol can be diff btwn Taylor and Euler
@@ -46,7 +46,7 @@ def test_linear(solver_to_test):
                 + 9 * (x**4) * f(x, y),
             ],
         )
-        y_prime.visualize("Taylor")
+        y_prime.visualize("Taylor", r"$6x^2 - 3x^2y$")
         
     y_prime_solutionlist = y_prime.iterations
 
@@ -70,35 +70,34 @@ def test_nonlinear(solver_to_test):
         
     num_steps = 5000
     x0 = 0
-    y0 = float(10/12)
-    #this makes C = 0.2
+    y0 = 0.5
+    #this makes C = 1
     xn = 4
     
     if solver_to_test == "Euler":
         y_prime = EulerSolver(f)
         rel_tol = 0.005
         y_prime.solve(x0, y0, xn, num_steps)
-        y_prime.visualize("Euler", "xy^2 - y")
+        y_prime.visualize("Euler", r"$xy^2 - y$")
     elif solver_to_test == 'Taylor':
         y_prime = TaylorSolver(f)
-        rel_tol = 0.005 #tolerance can be different between Taylor and Euler
+        rel_tol = 0.005 #tol can be diff btwn Taylor and Euler
         y_prime.solve(x0, 
                       y0, 
                       xn, 
                       num_steps,
                       [
                           lambda x, y:  y**2 + (2*x*y -1)*(x*(y**2) - y),
-                          lambda x, y:  4*x*(y**3) - 3*(y**2) + 
-                                        (6*(x*y)**2 - 6*x*y +1)*(x*(y**2) - y),
+                          lambda x, y:  (2*y)*(x*y**2 - y) + (2*x*y - 1)*(y**2),
                       ],
         )
 
-        y_prime.visualize("Taylor", "xy^2 - y")
+        y_prime.visualize("Taylor", r"$xy^2 - y$")
 
     y_prime_solutionlist = y_prime.iterations
 
     for step in range(num_steps):
         x = ((xn - x0)/num_steps)*step
-        y = 1/(-x + 1 + 0.2*math.exp(x))
+        y = 1/(1 + x + math.exp(x))
         assert check_tuple_close(y_prime_solutionlist[step], (x, y, f(x,y)), rel_tol)
 
