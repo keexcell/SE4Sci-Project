e385235e64aa94e4790166e12d69c3ff214e494f
Kayleigh Excell <kexcell@wisc.edu>
2025-07-16 09:50:49 -0500

    Made edits based on PR 1

--- tests/test_infinities.py
+++ tests/test_infinities.py
@@ -5,9 +5,8 @@ import math
 import abc
 
 
-#@pytest.mark.parametrize('solver_to_test', ['Euler', 'Taylor'])
-#def test_divergence(solver_to_test):
-def test_divergence():
+@pytest.mark.parametrize('solver_to_test', ['Euler', 'Taylor'])
+def test_divergence(solver_to_test):
     '''
     Now trying 2 variables, still simpler: f(x,y) = y'(x) = y*tan(x)
     Expect the answer to match y(x) = 1/cos(x)
@@ -17,14 +16,14 @@ def test_divergence():
     Second, just go to pi/3 so its a closed interval
     '''
     num_steps = 5000
-    rel_tol = 0.005
 
-    #if solver_to_test == 'Euler':
-    y_prime = EulerSolver(lambda x, y : y*math.tan(x))
-    #elif solver_to_test == 'Taylor':
-        #y_prime = TaylorSolver(lambda x, y : y*math.tan(x))
-        #rel_tol = 1e-9
-    
+    if solver_to_test == 'Euler':
+        y_prime = EulerSolver(lambda x, y : y*math.tan(x))
+        rel_tol = 0.005
+    elif solver_to_test == 'Taylor':
+        y_prime = TaylorSolver(lambda x, y : y*math.tan(x))
+        rel_tol = 0.005 #tol can be diff btwn Taylor and Euler  
+
     #there's cases where cos = 0 so y = 1/0 and there will be an error
     with pytest.raises(ValueError):
         y_prime.solve(0.0, 1.0, 2*math.pi, num_steps)
